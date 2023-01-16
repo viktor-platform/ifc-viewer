dc0a94d42db1abf4e1339371d555720d63943ec5
yannick <yannickmacken@gmail.com>
2023-01-16 13:37:01 +0100

    Fix transformation matrix

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from viktor.geometry import Triangle, TriangleAssembly, Material
 from viktor.parametrization import ViktorParametrization, FileField, MultiSelectField
 from viktor.views import GeometryView, GeometryResult
 
-
 ELEMENT_OPTIONS = {
     'IfcWall': Color(255, 204, 153),
     'IfcRoof': Color(255, 153, 153),
@@ -19,7 +18,7 @@ ELEMENT_OPTIONS = {
 
 
 class Parametrization(ViktorParametrization):
-    ifc_upload = FileField("Upload model", file_types=[".ifc"], max_size=10_000_000)
+    ifc_upload = FileField("Upload model", file_types=[".ifc"], max_size=50_000_000)
     element_filter = MultiSelectField(
         "Filter elements", options=list(ELEMENT_OPTIONS.keys())
     )
@@ -34,7 +33,7 @@ class Controller(ViktorController):
     def ifc_view(params, **kwargs):
         """View 3D model of uploaded .ifc file."""
 
-        # Load ifc upload into model
+        # Load ifc file into model
         ifc_upload: File = params.ifc_upload.file
         temp_file = NamedTemporaryFile(suffix='.sld', delete=False, mode='wb')
         temp_file.write(ifc_upload.getvalue_binary())
@@ -56,7 +55,6 @@ class Controller(ViktorController):
             )
             elements.extend(model.by_type(element_type))
             for element in elements:
-
                 # Create triangle assembly and assign material
                 triangle_assembly = TriangleAssembly(
                     triangles=get_faces_from_ifc_element(element, settings),
@@ -73,44 +71,32 @@ def get_faces_from_ifc_element(element, settings) -> List[Triangle]:
     # Get ifc element geometry
     shape = ifcopenshell.geom.create_shape(settings, element)
     faces = shape.geometry.faces
-    verts = shape.geometry.verts
 
-    # A 4x4 matrix representing the location and rotation of the element, in the form:
-    # [ [ x_x, y_x, z_x, x   ]
-    #   [ x_y, y_y, z_y, y   ]
-    #   [ x_z, y_z, z_z, z   ]
-    #   [ 0.0, 0.0, 0.0, 1.0 ] ]
-    # The position is given by the last column: (x, y, z)
-    # The rotation is described by the first three columns,
-    # by explicitly specifying the local X, Y, Z axes.
-    # The first column is a normalised vector of the local X axis: (x_x, x_y, x_z)
-    # The second column is a normalised vector of the local Y axis: (y_x, y_y, y_z)
-    # The third column is a normalised vector of the local Z axis: (z_x, z_y, z_z)
-    # The axes follow a right-handed coordinate system.
-    # Objects are never scaled, so the scale factor of the matrix is always 1.
+    # Convert IfcOpenShell matrix to Shapely matrix
     matrix = shape.transformation.matrix.data
-    shapely_points = []
-    for i in range(0, len(verts), 3):
-        point = affinity.affine_transform(
-            Point(
-                verts[i],
-                verts[i + 1],
-                verts[i + 2]
-            ),
-            matrix,
-        )
-        shapely_points.append(point)
+    shapely_matrix = [
+        matrix[0], matrix[3], matrix[6],
+        matrix[1], matrix[4], matrix[7],
+        matrix[2], matrix[5], matrix[8],
+        matrix[9], matrix[10], matrix[11],
+    ]
 
+    # Transform all vertices with transformation matrix
+    verts = shape.geometry.verts
     grouped_verts = []
-    for point in shapely_points:
+    for i in range(0, len(verts), 3):
+        point = Point(verts[i], verts[i + 1], verts[i + 2])
+        point = affinity.affine_transform(point, shapely_matrix)
         grouped_verts.append(geometry.Point(point.x, point.y, point.z))
 
-    grouped_faces = [
-        Triangle(
+    # Convert vertices of each face to triangle
+    grouped_faces = []
+    for i in range(0, len(faces), 3):
+        triangle = Triangle(
             grouped_verts[faces[i]],
             grouped_verts[faces[i + 1]],
             grouped_verts[faces[i + 2]]
         )
-        for i in range(0, len(faces), 3)
-    ]
+        grouped_faces.append(triangle)
+
     return grouped_faces
