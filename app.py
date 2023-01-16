43a7d17cf9c153fac85edeff9da1bed943b8a074
yannick <yannickmacken@gmail.com>
2023-01-16 15:27:40 +0100

    Dynamic element list, view colors

--- app.py
+++ app.py
@@ -1,60 +1,67 @@
+import random
 from pathlib import Path
+from random import randint
 from tempfile import NamedTemporaryFile
-from typing import List
+from typing import List, Any
 
 import ifcopenshell
 import ifcopenshell.geom as geom
 from shapely import affinity, Point
-from viktor import ViktorController, File, Color, geometry
+from viktor import ViktorController, File, Color, geometry, UserException
 from viktor.geometry import Triangle, TriangleAssembly, Material
-from viktor.parametrization import ViktorParametrization, FileField, MultiSelectField
+from viktor.parametrization import ViktorParametrization, FileField, MultiSelectField, \
+    LineBreak
 from viktor.views import GeometryView, GeometryResult
 
-ELEMENT_OPTIONS = {
-    'IfcWall': Color(255, 204, 153),
-    'IfcRoof': Color(255, 153, 153),
-    'IfcSlab': Color(128, 128, 128)
-}
+
+def get_element_options(params, **kwargs) -> List[str]:
+    """Get all existing geometry element types from ifc file."""
+    if not params.ifc_upload:
+        return []
+    model = load_ifc_file_into_model(params.ifc_upload.file)
+    elements = model.by_type("IfcElement")
+    element_options = []
+    for element in elements:
+        if element.Representation:
+            element_options.append(element.get_info()["type"])
+    return list(set(element_options))
 
 
 class Parametrization(ViktorParametrization):
-    ifc_upload = FileField("Upload model", file_types=[".ifc"], max_size=50_000_000)
-    element_filter = MultiSelectField(
-        "Filter elements", options=list(ELEMENT_OPTIONS.keys())
-    )
+    """Viktor parametrization."""
+    ifc_upload = FileField("Upload model", file_types=[".ifc"], max_size=20_000_000)
+    lb = LineBreak()
+    element_filter = MultiSelectField("Filter elements", options=get_element_options)
 
 
 class Controller(ViktorController):
+    """Viktor Controller."""
+
     label = 'My Entity Type'
     parametrization = Parametrization
 
     @staticmethod
-    @GeometryView("3D model", duration_guess=12)
+    @GeometryView("3D model of filtered elements", duration_guess=12)
     def ifc_view(params, **kwargs):
-        """View 3D model of uploaded .ifc file."""
+        """View 3D model of filtered elements from uploaded .ifc file."""
 
-        # Load ifc file into model
-        ifc_upload: File = params.ifc_upload.file
-        temp_file = NamedTemporaryFile(suffix='.sld', delete=False, mode='wb')
-        temp_file.write(ifc_upload.getvalue_binary())
-        temp_file.close()
-        path = Path(temp_file.name)
-        model = ifcopenshell.open(path)
-
-        # Settings
-        selected_elements = params.element_filter
-        elements = []
-        geometry_groups = []
+        # Load ifc file and set settings
+        if not params.ifc_upload or not params.element_filter:
+            raise UserException("Upload ifc file and select elements.")
+        model = load_ifc_file_into_model(params.ifc_upload.file)
         settings = ifcopenshell.geom.settings()
 
         # Get geometry from selected elements
+        geometry_groups = []
+        selected_elements = params.element_filter
         for element_type in selected_elements:
             material = Material(
                 name=element_type,
-                color=ELEMENT_OPTIONS[element_type]
+                color=get_random_color(element_type)
             )
-            elements.extend(model.by_type(element_type))
+            elements = model.by_type(element_type)
             for element in elements:
+
                 # Create triangle assembly and assign material
                 triangle_assembly = TriangleAssembly(
                     triangles=get_faces_from_ifc_element(element, settings),
@@ -100,3 +107,21 @@ def get_faces_from_ifc_element(element, settings) -> List[Triangle]:
         grouped_faces.append(triangle)
 
     return grouped_faces
+
+
+def load_ifc_file_into_model(file: File) -> Any:
+    """Load ifc file into ifc model object."""
+    ifc_upload: File = file
+    temp_file = NamedTemporaryFile(suffix='.sld', delete=False, mode='wb')
+    temp_file.write(ifc_upload.getvalue_binary())
+    temp_file.close()
+    path = Path(temp_file.name)
+    model = ifcopenshell.open(path)
+    return model
+
+
+def get_random_color(seed_word: str) -> Color:
+    """Generate pseudo random rgb color.
+    Will always return same color for same name."""
+    random.seed(seed_word)
+    return Color(randint(128, 220), (randint(100, 128)), randint(100, 220))
