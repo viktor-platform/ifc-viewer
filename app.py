00af7ee7bcf5caf8afd4032b41c04bd3c417fe99
NagelsThomas <123568967+NagelsThomas@users.noreply.github.com>
2023-01-30 09:52:38 +0100

    Revert "Update thomas"

--- app.py
+++ app.py
@@ -2,36 +2,23 @@ import random
 from pathlib import Path
 from random import randint
 from tempfile import NamedTemporaryFile
-from typing import Any, List
+from typing import List, Any
 
 import ifcopenshell
-from shapely import Point, affinity
-from viktor import Color, File, UserException, ViktorController, geometry
-from viktor.geometry import Material, Triangle, TriangleAssembly
-from viktor.parametrization import (
-    BooleanField,
-    DownloadButton,
-    FileField,
-    LineBreak,
-    MultiSelectField,
-    Text,
-    ViktorParametrization,
-)
-from viktor.result import DownloadResult
-from viktor.views import GeometryResult, GeometryView
+import ifcopenshell.geom as geom
+from shapely import affinity, Point
+from viktor import ViktorController, File, Color, geometry, UserException
+from viktor.geometry import Triangle, TriangleAssembly, Material
+from viktor.parametrization import ViktorParametrization, FileField, MultiSelectField, \
+    LineBreak, Text
+from viktor.views import GeometryView, GeometryResult
 
 
 def get_element_options(params, **kwargs) -> List[str]:
     """Get all existing geometry element types from ifc file."""
-    if not params.ifc_upload and not params.get_sample_ifc_toggle:
+    if not params.ifc_upload:
         return []
-    if params.get_sample_ifc_toggle:
-        params.sample_ifc = File.from_path(
-            Path(__file__).parent / "AC20-FZK-Haus (Sample IFC).ifc"
-        )
-        model = load_ifc_file_into_model(params.sample_ifc)
-    else:
-        model = load_ifc_file_into_model(params.ifc_upload.file)
+    model = load_ifc_file_into_model(params.ifc_upload.file)
     elements = model.by_type("IfcElement")
     element_options = []
     for element in elements:
@@ -42,7 +29,6 @@ def get_element_options(params, **kwargs) -> List[str]:
 
 class Parametrization(ViktorParametrization):
     """Viktor parametrization."""
-
     text1 = Text(
         """
 # Welcome to the ifc-viewer app!
@@ -62,19 +48,7 @@ The app is tested with IFC 4 files. For reference, check out some
 [example files](https://www.ifcwiki.org/index.php?title=KIT_IFC_Examples).
         """
     )
-
-    ifc_upload = FileField(
-        "Upload model",
-        file_types=[".ifc"],
-        max_size=20_000_000,
-    )
-
-    get_sample_ifc_toggle = BooleanField(
-        "Use sample IFC File",
-        default=False,
-        flex=30,
-    )
-
+    ifc_upload = FileField("Upload model", file_types=[".ifc"], max_size=20_000_000)
     lb = LineBreak()
     text2 = Text(
         """
@@ -84,42 +58,14 @@ Only elements existing in the IFC file can be selected.
 Geometry of selected elements will be shown in the 3D viewer.
         """
     )
-    element_filter = MultiSelectField(
-        "Filter elements",
-        options=get_element_options,
-    )
-
-    lb = LineBreak()
-    text3 = Text(
-        """
-## Download
-Only selected elements will be downloaded, this allows for easy removal of any of the elements
-        """
-    )
-    download = DownloadButton(
-        "Download",
-        method="download_file",
-    )
+    element_filter = MultiSelectField("Filter elements", options=get_element_options)
 
 
 class Controller(ViktorController):
     """Viktor Controller."""
 
-    label = "My Entity Type"
+    label = 'My Entity Type'
     parametrization = Parametrization
-
-    def download_file(self, params, **kwargs):
-        model = load_ifc_file_into_model(params.ifc_upload.file)
-        # remove all other parts from the ifc file which are not viewed
-        for element in model.by_type("IfcElement"):
-            if element.get_info()["type"] not in params.element_filter:
-                model.remove(element)
-        # part where we save the model as seen in the viewer
-        temp_file = NamedTemporaryFile(suffix=".ifc", delete=False, mode="wb")
-        model.write(str(Path(temp_file.name)))
-        temp_file.close()
-        path_out = Path(temp_file.name)
-        return DownloadResult(path_out.read_bytes(), "filtered_elements.ifc")
 
     @staticmethod
     @GeometryView("3D model of filtered elements", duration_guess=12)
@@ -127,29 +73,26 @@ class Controller(ViktorController):
         """View 3D model of filtered elements from uploaded .ifc file."""
 
         # Load ifc file and set settings
-        if not params.element_filter and params.get_sample_ifc_toggle == False:
+        if not params.ifc_upload or not params.element_filter:
             raise UserException("Upload ifc file and select elements.")
-        if params.get_sample_ifc_toggle == True:
-            params.sample_ifc = File.from_path(
-                Path(__file__).parent / "AC20-FZK-Haus (Sample IFC).ifc"
-            )
-            model = load_ifc_file_into_model(params.sample_ifc)
-        else:
-            model = load_ifc_file_into_model(params.ifc_upload.file)
+        model = load_ifc_file_into_model(params.ifc_upload.file)
         settings = ifcopenshell.geom.settings()
 
         # Get geometry from selected elements
         geometry_groups = []
         selected_elements = params.element_filter
         for element_type in selected_elements:
-            material = Material(name=element_type, color=get_random_color(element_type))
+            material = Material(
+                name=element_type,
+                color=get_random_color(element_type)
+            )
             elements = model.by_type(element_type)
             for element in elements:
 
                 # Create triangle assembly and assign material
                 triangle_assembly = TriangleAssembly(
                     triangles=get_faces_from_ifc_element(element, settings),
-                    material=material,
+                    material=material
                 )
                 geometry_groups.append(triangle_assembly)
 
@@ -166,18 +109,10 @@ def get_faces_from_ifc_element(element, settings) -> List[Triangle]:
     # Convert IfcOpenShell matrix to Shapely matrix
     matrix = shape.transformation.matrix.data
     shapely_matrix = [
-        matrix[0],
-        matrix[3],
-        matrix[6],
-        matrix[1],
-        matrix[4],
-        matrix[7],
-        matrix[2],
-        matrix[5],
-        matrix[8],
-        matrix[9],
-        matrix[10],
-        matrix[11],
+        matrix[0], matrix[3], matrix[6],
+        matrix[1], matrix[4], matrix[7],
+        matrix[2], matrix[5], matrix[8],
+        matrix[9], matrix[10], matrix[11],
     ]
 
     # Transform all vertices with transformation matrix
@@ -194,7 +129,7 @@ def get_faces_from_ifc_element(element, settings) -> List[Triangle]:
         triangle = Triangle(
             grouped_verts[faces[i]],
             grouped_verts[faces[i + 1]],
-            grouped_verts[faces[i + 2]],
+            grouped_verts[faces[i + 2]]
         )
         grouped_faces.append(triangle)
 
@@ -204,7 +139,7 @@ def get_faces_from_ifc_element(element, settings) -> List[Triangle]:
 def load_ifc_file_into_model(file: File) -> Any:
     """Load ifc file into ifc model object."""
     ifc_upload: File = file
-    temp_file = NamedTemporaryFile(suffix=".sld", delete=False, mode="wb")
+    temp_file = NamedTemporaryFile(suffix='.sld', delete=False, mode='wb')
     temp_file.write(ifc_upload.getvalue_binary())
     temp_file.close()
     path = Path(temp_file.name)
